4e9495cc4d6e805bcb7025ac47c590b34b5a95cd
Илья Чернышев <chernyshev@retailcrm.ru>
2026-06-25 13:44:15 +0500

    Fix metric color selection

--- src/ui/units/wizard/actions/dialog.ts
+++ src/ui/units/wizard/actions/dialog.ts
@@ -168,7 +168,7 @@ export function openDialogMetric({extraSettings, onApply}: OpenDialogMetricArgum
                         //           };
 
                         const metricSettins = {
-                            metricFontColorIndex: colorIndex,
+                            metricFontColorIndex: color ? undefined : colorIndex,
                             metricFontSize: size,
                             metricFontColorPalette: palette,
                             metricFontColor: color,
